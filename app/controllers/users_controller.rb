2ed9fb5aa38d9d62ccd7512426c5ad40a4694603
Jamezjaz <jamezjaz@gmail.com>
2020-07-16 10:56:43 -0500

    Add user model and sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+      def show
+        @user = User.find(params[:id])
+      end
+    
+      def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to user_path(@user)
+        end
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:username, :email)
+      end
+end
