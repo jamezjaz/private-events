350dd7b805ef7da2c783098c973b3fc2e75d1d22
Jamezjaz <jamezjaz@gmail.com>
2020-07-16 12:27:44 -0500

    Adds a sign_in form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def new
+        session.reset
+        redirect_to user_path(session[:current_user_id]) if session[:current_user]
+    end
+    
+    def create
+        @user = User.find_by(username: params[:username])
+        if @user
+          session[:current_user] = @user.username
+          session[:current_user_id] = @user.id
+          redirect_to user_path(@user.id)
+        end
+      end
+    
+      def destroy
+        session.destroy
+        redirect_to new_sessions_path
+      end
+end
